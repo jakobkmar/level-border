fc028100fd6053b4a7d36cbf2e8c0f515c1e7928
Jakob K <bluefireolymp@gmail.com>
2021-12-14 16:17:48 +0100

    Implement forge mod

--- level-border-forge/build.gradle.kts
+++ level-border-forge/build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     `java-script`
     `forge-script`
 }
+
+dependencies {
+    implementation(project(":${rootProject.name}-common", configuration = "namedElements"))
+}
---
+++ level-border-forge/src/main/java/net/axay/levelborder/forge/LevelBorderMod.java
@@ -0,0 +1,28 @@
+package net.axay.levelborder.forge;
+
+import net.axay.levelborder.common.LevelBorderHandler;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerXpEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("level-border")
+public class LevelBorderMod {
+    public LevelBorderMod() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LevelBorderHandler.currentHandler = new LevelBorderHandler();
+    }
+
+    @SubscribeEvent
+    public void onChangeLevel(PlayerXpEvent.LevelChange event) {
+        if (event.getPlayer() instanceof ServerPlayer player) {
+            LevelBorderHandler.currentHandler.updateWorldBorder(player);
+        }
+    }
+}
