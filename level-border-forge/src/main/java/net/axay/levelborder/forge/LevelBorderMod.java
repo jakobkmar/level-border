d53cba9246b2e445619debde18f54e45474a5a80
Jakob K <bluefireolymp@gmail.com>
2021-12-14 16:31:59 +0100

    Add onPlayerJoin to Forge LevelBorderMod

--- level-border-forge/src/main/java/net/axay/levelborder/forge/LevelBorderMod.java
+++ level-border-forge/src/main/java/net/axay/levelborder/forge/LevelBorderMod.java
@@ -3,6 +3,7 @@ package net.axay.levelborder.forge;
 import net.axay.levelborder.common.LevelBorderHandler;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerXpEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,6 +18,13 @@ public class LevelBorderMod {
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
         LevelBorderHandler.currentHandler = new LevelBorderHandler();
+    }
+
+    @SubscribeEvent
+    public void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getPlayer() instanceof ServerPlayer player) {
+            LevelBorderHandler.currentHandler.initBorder(player);
+        }
     }
 
     @SubscribeEvent
